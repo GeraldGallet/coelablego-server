eea877e0e4ef16ef6e195f6237138dabb9eb0ad1
RemiW <remi.willems59@gmail.com>
2018-11-15 15:49:07 +0100

    Changed favicon for Yncrea logo

--- interface/src/app/app.module.ts
+++ interface/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ViewerComponent } from './viewer/viewer.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ViewerComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
