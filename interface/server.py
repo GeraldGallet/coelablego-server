a7b40f10ba26a2cb78b8a75db871f2e2cb5a3c60
GeraldGallet <gerald.gallet@isen.yncrea.fr>
2018-11-15 10:54:04 +0100

    Preparing the API and OpenCV structure

--- interface/server.py
+++ interface/server.py
@@ -14,19 +14,19 @@ CORS(app)
 
 @app.route("/")
 def hello():
-  return jsonify({'text': 'Hello World!'})
-  #return 'Hello World!'
+    return jsonify({'text': 'Hello World!'})
+    #return 'Hello World!'
 
 
 class Employees(Resource):
-  def get(self):
-	return {'employees': [{'id':1, 'name':'Balram'},{'id':2, 'name':'Tom'}]}
+    def get(self):
+        return {'employees': [{'id':1, 'name':'Balram'},{'id':2, 'name':'Tom'}]}
 
 class Employees_Name(Resource):
-  def get(self, employee_id):
-	print('Employee id:' + employee_id)
-	result = {'data': {'id':1, 'name':'Balram'}}
-	return jsonify(result)
+    def get(self, employee_id):
+        print('Employee id:' + employee_id)
+        result = {'data': {'id':1, 'name':'Balram'}}
+        return jsonify(result)
 
 
 api.add_resource(Employees, '/employees')
@@ -34,4 +34,4 @@ api.add_resource(Employees_Name, '/employees/<employee_id>')
 
 
 if __name__ == '__main__':
-  app.run(port=5002)
+    app.run(port=5002)
