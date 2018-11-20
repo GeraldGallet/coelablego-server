41063708afef7cdd8252b678c47137d6615294ed
RemiW <remi.willems59@gmail.com>
2018-11-20 14:44:06 +0100

    Added search page route

--- interface/src/app/app-routing.module.ts
+++ interface/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { NotFoundErrorComponent } from './not-found-error/not-found-error.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { SearchpageComponent } from './searchpage/searchpage.component';
 
 const routes: Routes = [
   { path: 'home', component: HomepageComponent },
+  { path: 'search', component: SearchpageComponent },
   { path: '', component: HomepageComponent },
   { path: 'not-found', component: NotFoundErrorComponent},
   { path: '**', redirectTo: 'not-found'}
