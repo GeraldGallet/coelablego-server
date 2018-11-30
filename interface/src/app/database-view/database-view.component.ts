73cc1bb33c048b31c6336b9cd66ea5336f71e8a1
MatteFlorian <matteflorian@live.fr>
2018-11-30 03:20:55 +0100

    Update database-view module to receive data and send it to new module

--- interface/src/app/database-view/database-view.component.ts
+++ interface/src/app/database-view/database-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { SimulateDataService } from '../shared/services/simulatedata/simulateData.service';
+
 
 @Component({
   selector: 'app-database-view',
@@ -7,39 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatabaseViewComponent implements OnInit {
 
-  private bags = [
-    {
-      id: 1,
-      name: "Sac 1",
-      pieces: [101,102]
-    }
-  ]
+  nameShowed= "Pieces";
+  list: any[];
+  type: string;
+  listSubscription: Subscription;
 
-  nameShowed= "Pieces/Bags";
-
-  private pieces = [
-    {
-      id:101,
-      color:"#00FF00",
-      length: 12,
-      height: 4,
-      width: 12,
-      shape: "rectangle"
-    },
-    {
-      id:102,
-      color:"#0000FF",
-      diameter : 12,
-      shape: "circle"
-    }
-  ]
-
-  constructor() { }
+  constructor(private simulateDataService:SimulateDataService) { }
 
   ngOnInit() {
+    this.listSubscription = this.simulateDataService.databaseSubject.subscribe(
+      (list: any[]) => {
+        this.list = list;
+      }
+    );
+    this.updateList();
+  }
+
+  ngOnDestroy() {
+    this.listSubscription.unsubscribe();
   }
 
   affectName(value: string){
-    this.nameShowed = value
+    this.nameShowed = this.simulateDataService.affectName(value);
+    this.updateList();
+  }
+
+  updateList(){
+    this.simulateDataService.emitDatabaseSubject();
+    //Define if it's a bag or a piece
+    if (this.list[0].hasOwnProperty('pieces')) {   //not a very good thing because element 0 has to exist
+      // mean it's a piece
+      this.type = 'Bag';
+    }
+    else{
+      this.type = 'Piece';
+    }
   }
 }
--- interface/src/app/database-view/database-view.module.ts
+++ interface/src/app/database-view/database-view.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DatabaseViewComponent } from './database-view.component';
 import { MaterialModule } from 'src/app/material.module';
+import { DatabaseElementModule } from '../database-element/database-element.module';
 
 @NgModule({
   declarations: [DatabaseViewComponent],
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    DatabaseElementModule
   ]
 })
 export class DatabaseViewModule { }
