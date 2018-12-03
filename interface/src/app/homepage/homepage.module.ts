0a683cd233f925f3e847a6f42e886441be865c54
MatteFlorian <matteflorian@live.fr>
2018-12-03 17:21:11 +0100

    Homepage updated

--- interface/src/app/homepage/homepage.module.ts
+++ interface/src/app/homepage/homepage.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage.component';
+import { MaterialModule } from 'src/app/material.module';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [HomepageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    MatButtonModule
   ]
 })
 export class HomepageModule { }
